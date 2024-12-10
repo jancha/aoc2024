f2202a1474da577d1f56c399741b52767c78c449
Janis Volbergs <janis.volbergs@gmail.com>
2024-12-10 15:17:18 +0200

    Day 10, puzzle 19 and 20

---
+++ puzzle20/src/main.rs
@@ -0,0 +1,110 @@
+use std::fs;
+
+fn main() {
+    println!("{}", analyze("input.txt"));
+}
+
+const CHAR_0: u8 = 48;
+
+fn analyze(file: &str) -> usize {
+    let file = fs::read_to_string(file).expect("Could not read file?");
+    let map: Vec<&str> = file.trim().split("\n").collect();
+
+    let map_width = map.first().unwrap().len();
+    let map_height = map.len();
+
+    let map = file.replace("\n", "");
+    let map_binary = map.as_bytes();
+
+    let mut trails_found = 0;
+    map_binary.iter().enumerate().for_each(|(index, height)| {
+        let height: u8 = *height - CHAR_0;
+        if height == 0 {
+            let (x, y) = index_to_xy(&index, &map_width);
+
+            trails_found += explore_trails(None, &x, &y, map_binary, &map_width, &map_height);
+        }
+    });
+
+    trails_found
+}
+
+fn explore_trails(
+    prev_height: Option<&u8>,
+    x: &usize,
+    y: &usize,
+    map_binary: &[u8],
+    map_width: &usize,
+    map_height: &usize,
+) -> usize {
+    let index = xy_to_index(x, y, map_width);
+    let height = map_binary[index] - CHAR_0;
+
+    if let Some(prev_height) = prev_height {
+        let height_diff = height as i8 - *prev_height as i8;
+
+        if height_diff != 1 {
+            return 0;
+        }
+
+        if height == 9 {
+            return 1;
+        }
+    }
+
+    let mut trails_found = 0;
+    if *x > 0 {
+        trails_found += explore_trails(
+            Some(&height),
+            &(*x - 1),
+            y,
+            map_binary,
+            map_width,
+            map_height,
+        );
+    }
+    if *x < map_width - 1 {
+        trails_found += explore_trails(
+            Some(&height),
+            &(*x + 1),
+            y,
+            map_binary,
+            map_width,
+            map_height,
+        );
+    }
+    if *y > 0 {
+        trails_found += explore_trails(
+            Some(&height),
+            x,
+            &(*y - 1),
+            map_binary,
+            map_width,
+            map_height,
+        );
+    }
+    if *y < map_height - 1 {
+        trails_found += explore_trails(
+            Some(&height),
+            x,
+            &(*y + 1),
+            map_binary,
+            map_width,
+            map_height,
+        );
+    }
+    trails_found
+}
+
+fn index_to_xy(index: &usize, map_width: &usize) -> (usize, usize) {
+    (index % map_width, index / map_width)
+}
+fn xy_to_index(x: &usize, y: &usize, map_width: &usize) -> usize {
+    y * map_width + x
+}
+
+#[test]
+fn test_1() {
+    assert_eq!(analyze("test.txt"), 81);
+    assert_eq!(analyze("input.txt"), 1017);
+}
