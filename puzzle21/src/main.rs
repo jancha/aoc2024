f0a8c7117b74a70d61850db17aae2f1b21ffcc23
Janis Volbergs <janis@zoomcharts.com>
2024-12-11 21:27:20 +0200

    Day 10, puzzle 21 and 22

---
+++ puzzle21/src/main.rs
@@ -0,0 +1,58 @@
+use std::fs;
+
+fn main() {
+    println!("{}", analyze("input.txt", 25));
+}
+
+const CHAR_0: u8 = 48;
+
+fn analyze(file: &str, mutations: u8) -> usize {
+    let file = fs::read_to_string(file).expect("Could not read file?");
+    let map: Vec<&str> = file.trim().split(' ').collect();
+
+    let mut stones: Vec<usize> = map
+        .iter()
+        .map(|x| {
+            let y: usize = x.parse().unwrap();
+            y
+        })
+        .collect();
+
+    println!("{:#?}", stones);
+
+    for _i in 0..mutations {
+        let mut stones_new: Vec<usize> = vec![];
+
+        let len = stones.len();
+
+        for j in 0..len {
+            let stone = stones.get(j).unwrap();
+
+            match stone {
+                0 => stones_new.push(1),
+                x if (x.ilog(10_usize) + 1) % 2 == 0 => {
+                    let mid_dividor: usize = 10_usize.pow((x.ilog(10_usize) + 1) / 2);
+                    let left = x / mid_dividor;
+                    let right = x - left * mid_dividor;
+                    stones_new.push(left);
+                    stones_new.push(right);
+                }
+                x => {
+                    stones_new.push(x * 2024);
+                }
+            }
+        }
+        stones = stones_new;
+
+        println!("After {} iteration: {}", _i, stones.len());
+    }
+    stones.len()
+}
+
+#[test]
+fn test_1() {
+    //assert_eq!(analyze("test.txt", 6), 22);
+    assert_eq!(analyze("test.txt", 25), 55312);
+    assert_eq!(analyze("input.txt", 25), 228668);
+    assert_eq!(analyze("input.txt", 75), 0);
+}
