389b8554643cf4b160b70836560274b348e2691b
Janis Volbergs <janis@zoomcharts.com>
2025-01-14 19:53:38 +0200

    Optimised 35 & 36

--- puzzle35/src/main.rs
+++ puzzle35/src/main.rs
@@ -1,66 +1,17 @@
-use std::collections::HashMap;
 use std::fs;
 fn main() {
     println!("{}", analyze("input.txt", 71, 71, 1024));
 }
 
 const WALL: u8 = 35;
-const EMPTY: u8 = 46;
-
-#[derive(Debug, PartialEq)]
-enum Direction {
-    East,
-    West,
-    North,
-    South,
-}
-
-#[derive(Debug, PartialEq)]
-struct Tile {
-    start: bool,
-    finish: bool,
-    cost: usize,
-    left: bool,
-    right: bool,
-    up: bool,
-    down: bool,
-    x: usize,
-    y: usize,
-}
-
-impl Tile {
-    fn new(x: usize, y: usize, start: bool, finish: bool) -> Tile {
-        Tile {
-            start,
-            finish,
-            cost: 0,
-            left: false,
-            right: false,
-            up: false,
-            down: false,
-            x,
-            y,
-        }
-    }
-
-    fn link_or_create(&mut self, x: usize, y: usize) {
-        if x < self.x {
-            self.left = true;
-        } else if x > self.x {
-            self.right = true;
-        } else if y < self.y {
-            self.up = true;
-        } else if y > self.y {
-            self.down = true;
-        }
-    }
-}
 
 fn analyze(file: &str, map_width: usize, map_height: usize, read_len: usize) -> usize {
     let file = fs::read_to_string(file).expect("Could not read file?");
     let data: Vec<&str> = file.trim().split("\n").map(|x| x.trim()).collect();
-    let mut map_binary: Vec<u8> = vec![EMPTY; map_width * map_height];
+    let start_index = 0;
+    let end_index = map_width * map_height - 1;
 
+    let mut map_binary: [u8; 71 * 71] = [0; 71 * 71];
     for i in &data[0..read_len] {
         if i.is_empty() {
             continue;
@@ -74,147 +25,98 @@ fn analyze(file: &str, map_width: usize, map_height: usize, read_len: usize) -> 
         map_binary[y * map_width + x] = WALL;
     }
 
-    print_map(&map_binary, map_width);
-
-    let mut tiles: HashMap<usize, Tile> = HashMap::new();
-
-    build_graph(&map_binary, &map_width, &mut tiles);
-
-    // now perform path cost analysis, starting from the start node
-
-    let start_index = 0;
-    let end_index = map_width * map_height - 1;
-
     let mut min = 0;
+    let mut map_cost: [usize; 71 * 71] = [0; 71 * 71];
 
-    walk_graph(&mut min, start_index, end_index, &mut tiles, &map_width);
+    walk_graph(
+        &map_binary,
+        &mut map_cost,
+        &mut min,
+        start_index,
+        end_index,
+        &map_width,
+        &map_height,
+    );
 
     min
 }
 
 fn walk_graph(
+    map_binary: &[u8; 71 * 71],
+    map_cost: &mut [usize; 71 * 71],
     min: &mut usize,
     start: usize,
     end: usize,
-    tiles: &mut HashMap<usize, Tile>,
     map_width: &usize,
+    map_height: &usize,
 ) {
-    let tile = tiles.get(&start).unwrap();
-    let cost = tile.cost;
+    let cost = map_cost[start];
 
     if start == end {
         if *min > 0 {
-            *min = cost.min(*min);
+            if *min > cost {
+                *min = cost;
+            }
         } else {
             *min = cost;
         }
         return;
     }
 
-    let tile_right = tile.right;
-    let tile_left = tile.left;
-    let tile_up = tile.up;
-    let tile_down = tile.down;
-
     let mut explore = |new_index: usize| {
         let new_cost = cost + 1;
-        let next = tiles.get_mut(&new_index).unwrap();
-        let rule1 = (next.cost > new_cost && (new_cost < *min || *min == 0)) || next.cost == 0;
+        let next_cost = map_cost[new_index];
+        let rule1 = (next_cost > new_cost && (new_cost < *min || *min == 0)) || next_cost == 0;
         if rule1 {
-            next.cost = new_cost;
-            walk_graph(min, new_index, end, tiles, map_width);
-        }
-    };
-
-    if tile_right {
-        explore(start + 1);
-    }
-    if tile_left {
-        explore(start - 1);
-    }
-    if tile_up {
-        explore(start - map_width);
-    }
-    if tile_down {
-        explore(start + map_width);
-    }
-}
-
-fn build_graph(map_binary: &[u8], map_width: &usize, tiles: &mut HashMap<usize, Tile>) {
-    for (index, value) in map_binary.iter().enumerate() {
-        if *value != WALL {
-            let (x, y) = index_to_xy(&index, map_width);
-
-            let mut tile = Tile::new(
-                x,
-                y,
-                x == 0 && y == 0,
-                x == *map_width - 1 && y == *map_width - 1,
+            map_cost[new_index] = new_cost;
+            walk_graph(
+                map_binary, map_cost, min, new_index, end, map_width, map_height,
             );
-            // look around
-            if let Some(index) = get_neighbour(index, Direction::East, map_binary, map_width) {
-                let (x1, y1) = index_to_xy(&index, map_width);
-                tile.link_or_create(x1, y1);
-            }
-            if let Some(index) = get_neighbour(index, Direction::West, map_binary, map_width) {
-                let (x1, y1) = index_to_xy(&index, map_width);
-                tile.link_or_create(x1, y1);
-            }
-            if let Some(index) = get_neighbour(index, Direction::North, map_binary, map_width) {
-                let (x1, y1) = index_to_xy(&index, map_width);
-                tile.link_or_create(x1, y1);
-            }
-            if let Some(index) = get_neighbour(index, Direction::South, map_binary, map_width) {
-                let (x1, y1) = index_to_xy(&index, map_width);
-                tile.link_or_create(x1, y1);
-            }
-            tiles.insert(index, tile);
         }
-    }
-}
-
-fn print_map(map: &[u8], map_width: usize) {
-    for (index, val) in map.iter().enumerate() {
-        if index % map_width == 0 {
-            println!();
-        }
-        if *val == WALL {
-            print!("#");
-        } else {
-            print!(".");
-        }
-    }
-}
-fn get_neighbour(
-    index: usize,
-    direction: Direction,
-    map_binary: &[u8],
-    map_width: &usize,
-) -> Option<usize> {
-    let (x, y) = index_to_xy(&index, map_width);
-
-    let mut x1: isize = x as isize;
-    let mut y1: isize = y as isize;
-
-    match direction {
-        Direction::East => x1 -= 1,
-        Direction::West => x1 += 1,
-        Direction::North => y1 -= 1,
-        Direction::South => y1 += 1,
     };
 
-    if x1 >= *map_width as isize || x1 < 0 || y1 >= *map_width as isize || y1 < 0 {
-        return None;
-    }
+    let (x, y) = index_to_xy(&start, map_width);
 
-    let index = (y1 * *map_width as isize + x1) as usize;
-
-    if let Some(val) = map_binary.get(index) {
-        if *val == EMPTY {
-            return Some(index);
+    let is_tile = |dx: isize, dy: isize| -> Option<usize> {
+        if dx != 0 {
+            let nx = x as isize + dx;
+            if nx < 0 || nx >= *map_width as isize {
+                None
+            } else {
+                let index = ((y * *map_width) as isize + nx) as usize;
+                if map_binary[index] != WALL {
+                    Some(index)
+                } else {
+                    None
+                }
+            }
+        } else {
+            let ny = y as isize + dy;
+            if ny < 0 || ny >= *map_height as isize {
+                None
+            } else {
+                let index = ((ny * *map_width as isize) + x as isize) as usize;
+                if map_binary[index] != WALL {
+                    Some(index)
+                } else {
+                    None
+                }
+            }
         }
+    };
+
+    if let Some(index) = is_tile(1, 0) {
+        explore(index);
     }
-    None
+    if let Some(index) = is_tile(-1, 0) {
+        explore(index);
+    }
+    if let Some(index) = is_tile(0, -1) {
+        explore(index);
+    }
+    if let Some(index) = is_tile(0, 1) {
+        explore(index);
+    }
 }
 
 fn index_to_xy(index: &usize, map_width: &usize) -> (usize, usize) {
@@ -224,5 +126,4 @@ fn index_to_xy(index: &usize, map_width: &usize) -> (usize, usize) {
 #[test]
 fn test_1() {
     assert_eq!(analyze("test.txt", 7, 7, 12), 22);
-    //assert_eq!(analyze("input.txt"), 88468);
 }
--- puzzle36/src/main.rs
+++ puzzle36/src/main.rs
@@ -1,60 +1,9 @@
-use std::collections::HashMap;
 use std::fs;
 fn main() {
     println!("{}", analyze("input.txt", 71, 71, 1024));
 }
 
 const WALL: u8 = 35;
-const EMPTY: u8 = 46;
-
-#[derive(Debug, PartialEq)]
-enum Direction {
-    East,
-    West,
-    North,
-    South,
-}
-
-#[derive(Debug, PartialEq)]
-struct Tile {
-    start: bool,
-    finish: bool,
-    cost: usize,
-    left: bool,
-    right: bool,
-    up: bool,
-    down: bool,
-    x: usize,
-    y: usize,
-}
-
-impl Tile {
-    fn new(x: usize, y: usize, start: bool, finish: bool) -> Tile {
-        Tile {
-            start,
-            finish,
-            cost: 0,
-            left: false,
-            right: false,
-            up: false,
-            down: false,
-            x,
-            y,
-        }
-    }
-
-    fn link_or_create(&mut self, x: usize, y: usize) {
-        if x < self.x {
-            self.left = true;
-        } else if x > self.x {
-            self.right = true;
-        } else if y < self.y {
-            self.up = true;
-        } else if y > self.y {
-            self.down = true;
-        }
-    }
-}
 
 fn analyze(file: &str, map_width: usize, map_height: usize, read_len: usize) -> String {
     let file = fs::read_to_string(file).expect("Could not read file?");
@@ -72,14 +21,7 @@ fn analyze(file: &str, map_width: usize, map_height: usize, read_len: usize) -> 
     )
 }
 
-fn build_map(
-    data: &[&str],
-    map_width: usize,
-    from: usize,
-    to: usize,
-    tiles: &mut HashMap<usize, Tile>,
-    map: &mut [u8],
-) {
+fn build_map(data: &[&str], map_width: usize, from: usize, to: usize, map: &mut [u8]) {
     for i in &data[from..to] {
         if i.is_empty() {
             continue;
@@ -92,8 +34,6 @@ fn build_map(
 
         map[y * map_width + x] = WALL;
     }
-
-    build_graph(map, &map_width, tiles);
 }
 
 fn solve_quicksort(
@@ -111,20 +51,19 @@ fn solve_quicksort(
     let mut end = read_len + split;
     loop {
         let mut min = 0;
-        let mut min_path: Vec<usize> = vec![];
-        let mut tiles: HashMap<usize, Tile> = HashMap::new();
-        let mut map_binary: Vec<u8> = vec![EMPTY; map_width * map_height];
+        let mut map_binary: [u8; 71 * 71] = [0; 71 * 71];
+        let mut map_cost: [usize; 71 * 71] = [0; 71 * 71];
 
-        build_map(data, map_width, start, end, &mut tiles, &mut map_binary);
+        build_map(data, map_width, start, end, &mut map_binary);
 
         walk_graph(
+            &map_binary,
+            &mut map_cost,
             &mut min,
-            &mut min_path,
-            [].to_vec(),
             start_index,
             end_index,
-            &mut tiles,
             &map_width,
+            &map_height,
         );
 
         if min != 0 {
@@ -143,22 +82,20 @@ fn solve_quicksort(
 }
 
 fn walk_graph(
+    map_binary: &[u8; 71 * 71],
+    map_cost: &mut [usize; 71 * 71],
     min: &mut usize,
-    min_path: &mut Vec<usize>,
-    cur_path: Vec<usize>,
     start: usize,
     end: usize,
-    tiles: &mut HashMap<usize, Tile>,
     map_width: &usize,
+    map_height: &usize,
 ) {
-    let tile = tiles.get(&start).unwrap();
-    let cost = tile.cost;
+    let cost = map_cost[start];
 
     if start == end {
         if *min > 0 {
             if *min > cost {
                 *min = cost;
-                *min_path = cur_path.to_vec();
             }
         } else {
             *min = cost;
@@ -166,100 +103,60 @@ fn walk_graph(
         return;
     }
 
-    let tile_right = tile.right;
-    let tile_left = tile.left;
-    let tile_up = tile.up;
-    let tile_down = tile.down;
-
     let mut explore = |new_index: usize| {
         let new_cost = cost + 1;
-        let next = tiles.get_mut(&new_index).unwrap();
-        let rule1 = (next.cost > new_cost && (new_cost < *min || *min == 0)) || next.cost == 0;
-        let mut new_path = cur_path.clone();
-        new_path.push(new_index);
+        let next_cost = map_cost[new_index];
+        let rule1 = (next_cost > new_cost && (new_cost < *min || *min == 0)) || next_cost == 0;
         if rule1 {
-            next.cost = new_cost;
-            walk_graph(min, min_path, new_path, new_index, end, tiles, map_width);
-        }
-    };
-
-    if tile_right {
-        explore(start + 1);
-    }
-    if tile_left {
-        explore(start - 1);
-    }
-    if tile_up {
-        explore(start - map_width);
-    }
-    if tile_down {
-        explore(start + map_width);
-    }
-}
-
-fn build_graph(map_binary: &[u8], map_width: &usize, tiles: &mut HashMap<usize, Tile>) {
-    for (index, value) in map_binary.iter().enumerate() {
-        if *value != WALL {
-            let (x, y) = index_to_xy(&index, map_width);
-
-            let mut tile = Tile::new(
-                x,
-                y,
-                x == 0 && y == 0,
-                x == *map_width - 1 && y == *map_width - 1,
+            map_cost[new_index] = new_cost;
+            walk_graph(
+                map_binary, map_cost, min, new_index, end, map_width, map_height,
             );
-            // look around
-            if let Some(index) = get_neighbour(index, Direction::East, map_binary, map_width) {
-                let (x1, y1) = index_to_xy(&index, map_width);
-                tile.link_or_create(x1, y1);
-            }
-            if let Some(index) = get_neighbour(index, Direction::West, map_binary, map_width) {
-                let (x1, y1) = index_to_xy(&index, map_width);
-                tile.link_or_create(x1, y1);
-            }
-            if let Some(index) = get_neighbour(index, Direction::North, map_binary, map_width) {
-                let (x1, y1) = index_to_xy(&index, map_width);
-                tile.link_or_create(x1, y1);
-            }
-            if let Some(index) = get_neighbour(index, Direction::South, map_binary, map_width) {
-                let (x1, y1) = index_to_xy(&index, map_width);
-                tile.link_or_create(x1, y1);
-            }
-            tiles.insert(index, tile);
         }
-    }
-}
-
-fn get_neighbour(
-    index: usize,
-    direction: Direction,
-    map_binary: &[u8],
-    map_width: &usize,
-) -> Option<usize> {
-    let (x, y) = index_to_xy(&index, map_width);
-
-    let mut x1: isize = x as isize;
-    let mut y1: isize = y as isize;
-
-    match direction {
-        Direction::East => x1 -= 1,
-        Direction::West => x1 += 1,
-        Direction::North => y1 -= 1,
-        Direction::South => y1 += 1,
     };
 
-    if x1 >= *map_width as isize || x1 < 0 || y1 >= *map_width as isize || y1 < 0 {
-        return None;
-    }
+    let (x, y) = index_to_xy(&start, map_width);
 
-    let index = (y1 * *map_width as isize + x1) as usize;
-
-    if let Some(val) = map_binary.get(index) {
-        if *val == EMPTY {
-            return Some(index);
+    let is_tile = |dx: isize, dy: isize| -> Option<usize> {
+        if dx != 0 {
+            let nx = x as isize + dx;
+            if nx < 0 || nx >= *map_width as isize {
+                None
+            } else {
+                let index = ((y * *map_width) as isize + nx) as usize;
+                if map_binary[index] != WALL {
+                    Some(index)
+                } else {
+                    None
+                }
+            }
+        } else {
+            let ny = y as isize + dy;
+            if ny < 0 || ny >= *map_height as isize {
+                None
+            } else {
+                let index = ((ny * *map_width as isize) + x as isize) as usize;
+                if map_binary[index] != WALL {
+                    Some(index)
+                } else {
+                    None
+                }
+            }
         }
+    };
+
+    if let Some(index) = is_tile(1, 0) {
+        explore(index);
     }
-    None
+    if let Some(index) = is_tile(-1, 0) {
+        explore(index);
+    }
+    if let Some(index) = is_tile(0, -1) {
+        explore(index);
+    }
+    if let Some(index) = is_tile(0, 1) {
+        explore(index);
+    }
 }
 
 fn index_to_xy(index: &usize, map_width: &usize) -> (usize, usize) {
@@ -269,5 +166,4 @@ fn index_to_xy(index: &usize, map_width: &usize) -> (usize, usize) {
 #[test]
 fn test_1() {
     assert_eq!(analyze("test.txt", 7, 7, 12), "6,1");
-    //assert_eq!(analyze("input.txt"), 88468);
 }
